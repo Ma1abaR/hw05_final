2b0d05999f5bcf18638e8b43f55a482ccf02b6a5
Артур Степанов <extrimiti@yandex.ru>
2023-01-20 16:22:27 +0300

    ДÑрhw05 final

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -1,22 +1,18 @@
-from http import HTTPStatus
-
 from django.shortcuts import render
 
 
 def page_not_found(request, exception):
-    return render(request,
-                  'core/404.html', {'path': request.path},
-                  HTTPStatus.NOT_FOUND
-                  )
+    return render(request, 'core/404.html', {'path': request.path}, status=404)
 
 
 def server_error(request):
-    return render(request, 'core/500.html', HTTPStatus.INTERNAL_SERVER_ERROR)
+    return render(request, 'core/500.html', status=500)
 
 
 def permission_denied(request, exception):
-    return render(request, 'core/403.html', HTTPStatus.FORBIDDEN)
+    return render(request, 'core/403.html', status=403)
 
 
 def csrf_failure(request, reason=''):
-    return render(request, 'core/403csrf.html', HTTPStatus.FORBIDDEN)
+    return render(request, 'core/403csrf.html', status=403)
+
